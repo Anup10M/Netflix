f49bf34d02d3a7877f56cc68d66f7c7ed51d5f15
Anup10M <y10259234@gmail.com>
2025-03-21 09:27:44 +0545

    Add files via upload

--- Apflix.js
+++ Apflix.js
@@ -1,4 +1,35 @@
-setTimeout(function() {
-    document.getElementById("intro-video").style.display = "none";
-    document.getElementsByClassName("main").style.display = "block";
-}, 4400); 
+
+        const carousel=document.querySelector('.slide-images');
+        firstimg = carousel.querySelectorAll(".box-images")[0]; 
+        arrowIcon = document.querySelectorAll('.button-slide i');
+        let isdragstar=false,prevPageX, prevscrollleft;
+
+        let firstlmgwidth = firstimg.clientWidth + 14;
+       
+        arrowIcon.forEach(icon => {
+            icon.addEventListener("click", () =>{
+               carousel.scrollLeft += icon.id == "lf" ? -firstlmgwidth: firstlmgwidth
+            })
+        });
+        const dragstar=(e) =>{
+            isdragstar=true;
+            prevPageX=e.pageX;
+            prevscrollleft=carousel.scrollLeft;
+        }
+        document.querySelectorAll('.slide-images img').forEach(img => {
+         img.addEventListener('dragstart', (e) => e.preventDefault());
+        });
+
+
+        const dragging= (e) => {
+            if (!isdragstar) return;
+            e.preventDefault();
+            let positionDiff = e.pageX - prevPageX;
+            carousel.scrollLeft=prevscrollleft - positionDiff;
+        }   
+        const dragstop = () => {
+            isdragstar=false;
+        }
+        carousel.addEventListener('mousedown',dragstar);
+        carousel.addEventListener("mousemove",dragging)
+        carousel.addEventListener("mouseup",dragstop)
